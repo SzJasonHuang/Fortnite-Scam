59b0e4483180316300575b502e5499f3e3dc06f2
whuang45 <=>
2025-06-10 20:50:54 -0700

    Added loading bars

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
-function showWarning(event) {
+function showLoading(event) {
     event.preventDefault();
     document.querySelector(".phish-box").style.display = "none";
-    document.getElementById("warning").style.display = "block";
+  
+    const loadingDiv = document.getElementById("loading");
+    loadingDiv.style.display = "block";
+  
+    const progressBar = document.getElementById("progress-bar");
+    let progress = 0;
+  
+    const interval = setInterval(() => {
+      progress += 2;
+      progressBar.style.width = progress + "%";
+  
+      if (progress >= 100) {
+        clearInterval(interval);
+        loadingDiv.style.display = "none";
+        document.getElementById("warning").style.display = "block";
+      }
+    }, 50); 
   }
   
